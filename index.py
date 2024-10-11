4077c2e6c9fa2a47296e1d0013eba9dda9752dde
azik-gamer <honh0934@gmail.com>
2024-10-11 15:39:24 +0500

    This code is about my best friend

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print("Hello Worl")
+berilgan = ["Baxtbekda telefon yo'q", "unda telefon yo'q", "baxtbekda telefon yo'q"]
+savol = input("Baxtbekda qanday telefon bor?")
+if savol in berilgan:
+    print("Siz juda aqlli ekansaiz, tog'ri topdingiz")
+else:
+    print("Yanglishdingiz Baxtbekda telefon yo'q")
+
+
+
+telefon = ['iphone', 'samsung', 'redmi', 'nokio']
+question = input("Baxtbek ga qanday telefon sovg'a qilmoqchisiz?")
+if question in telefon:
+    print("Yaxshi tanlov u bunga munosib!")
+else:
+    print("Menimcha bu yaxshi tanlov emas") 
